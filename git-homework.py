b71b7a6050a45f7b019e70fc267a584fbb7963c0
babadu14 <andriamanagadze7@gmail.com>
2024-10-06 00:18:38 +0400

    added revrse string function, string_to_dictionary function, duplicate letters in a word function

--- git-homework.py
+++ git-homework.py
@@ -3,13 +3,43 @@ def word(text):
         return False
     text = list(text)
     text.sort()
-    for i in range(len(text)):
-        for j in range(i+1, len(text)):
-            if text[i] == text[j]:
-                return False
+    # for i in range(len(text)):
+    #     for j in range(i+1, len(text)):
+    #         if text[i] == text[j]:
+    #             return False
+    # return True
+    l,r = 0, 1
+    while len(text) > r and len(text) > l:
+        if text[l] == text[r]:
+            return False
+        l +=1
+        r += 1
     return True
     
-print(word('hello'))
+print(word('telfoni'))
 
 
-#comm
+def reverse_string(string):
+    if not isinstance(string, str):
+        return False
+    str1 = ''
+    index = len(string)
+    while index > 0:
+        str1 += string[index - 1]
+        index -=1 
+    return str1
+print(reverse_string('hello'))
+
+
+def string_to_dictioanry(strg):
+    if not isinstance(strg, str):
+        return False
+    dic = {}
+    for letter in strg:
+        if letter in dic:
+            dic[letter] += 1
+        else:
+            dic[letter] = 1
+    return dic
+    
+print(string_to_dictioanry('hello'))
